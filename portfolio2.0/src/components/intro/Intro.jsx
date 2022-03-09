f8c2d2a3219d54577c3133d895c5dfcc763ad607
Nkepers <nick.kepers.coder@gmail.com>
2022-03-09 15:02:33 -0700

    intro section temp finished

--- portfolio2.0/src/components/intro/Intro.jsx
+++ portfolio2.0/src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
 import './intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+  const textRef = useRef()
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay:  1000,
+      backSpeed: 70,
+      strings: ['Designer', 'Developer', 'Engineer'],
+    })
+  }, []);
+
   return (
     <div className='intro' id='intro'>
-      
+      <div className='left'>
+        <div className="imgContainer">
+          <img src='assets/portrait.png' alt='myPortrait' />
+        </div>
+      </div>
+
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hello there, I am</h2>
+          <h1>Nicholas Kepers</h1>
+          <h3>
+            Software <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/downArrow.png" alt="downArrow" />
+        </a>
+      </div>
     </div>
   )
 }
